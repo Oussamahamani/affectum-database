8bc50c717098b6733b7ef32dc66abd389ad1dba2
oussamahamani <oussamahamani2000@gmail.com>
2022-03-22 14:20:19 -0400

    adding post method and get method for a specif country

--- routes/countries.js
+++ routes/countries.js
@@ -13,6 +13,29 @@ router.get('/', async(req, res) => {
     }
 })
 
+//post method
+router.post('/', async (req,res)=>{
+    const post = new Post({
+        emotion: req.body.emotion,
+        country: req.body.country,
+        state: req.body.state,
+        city: req.body.city
+    });
+    try{
+        const savedPost = await post.save();
+        res.json(savedPost);
+    }catch(err){
+        res.json({message:err})
+    }
+})
 
-
+//get results for specifc country
+router.get('/:postId', async (req,res)=>{
+    try{
+        const post = await Post.find({country:req.params.postId})
+        res.json(post);
+    }catch(err){
+        console.log(err)
+    }
+})
 module.exports = router;
